a55afce95432d5a503b696886ce50480afb74450
jblasco <javier.blasco.herrera@gmail.com>
2013-09-24 21:13:49 +0200

    Use skimage.filter for the median filter. Much faster.

--- median_filter.py
+++ median_filter.py
@@ -9,32 +9,29 @@ import sys
 import argparse
 import repipy.utilities as utils
 import astropy.io.fits as fits
-
-def apply_median_filter(im, side):
-    """ Filter a masked np.ma array with a median filter. """
-    lx, ly = im.shape
-    mask = numpy.zeros((lx,ly),dtype=numpy.int0)
-    data = numpy.asarray(mask, dtype=numpy.float)
-    filt_im = numpy.ma.array(data, mask=mask)
-    for ii in range(lx):
-      for jj in range(ly):
-          minx, maxx = max([ii-side/2,0]), min([ii+side/2+1,lx])
-          miny, maxy = max([jj-side/2,0]), min([jj+side/2+1,ly])
-          filt_im[ii,jj] = numpy.ma.median(im[minx:maxx,miny:maxy])
-    return filt_im            
-            
+import skimage.filter
+          
 def filter_image(args):
     """ Routine that uses a median filter on a list of masked images. """
     output_list = []
     for image in args.input:
+        # Read image, mask and header
         im = utils.read_image_with_mask(image, mask_keyword=args.mask_key)
-        filt_im = apply_median_filter(im,args.side)
-        filt_im = filt_im.filled(args.fill_val)
+        hdr = fits.getheader(image)
+        
+        # skimage uses masks where 1 means valid and 0 invalid
+        mask = (im.mask + 1) % 2
+        filt_im = skimage.filter.median_filter(im.data, mask=mask, radius=args.radius)
+        
+        # Make name of file if not given
         if args.output == "":
             output = utils.add_suffix_prefix(image, suffix='-mf')
         else :
             output = args.output
-        fits.writeto(output, filt_im)
+        
+        # Add history line to the header and write to file
+        hdr.add_history("- Image median filtered. Radius = " + str(args.radius))
+        fits.writeto(output, filt_im, header=hdr)
         output_list.append(output)
     return output_list
     
@@ -49,16 +46,13 @@ parser.add_argument("--mask_key", metavar="mask_key", dest='mask_key', \
                     action='store', default="", help=' Keyword in the header ' +\
                     'of the image that contains the name of the mask. The mask '+\
                     'will contain ones (1) in those pixels to be MASKED OUT.')
-parser.add_argument("--side", metavar="side", type=int, dest="side", 
-                    action='store', required=True, help=" Size of the box to "+\
-                    "be used to filter the image. Mandatory argument.")
+parser.add_argument("--radius", metavar="radius", type=int, dest="radius", 
+                    action='store', required=True, help=" Radius of circle "+\
+                    "to be used to filter the image. Mandatory argument.")
 parser.add_argument("--output", metavar='output', dest='output', action='store',
                    default='', help='output image in which to save the result.'+\
                     ' If not provided the suffix -mf will be added to '+\
-                    'the input.')
-parser.add_argument("--fill_val", metavar="fill_val", dest="fill_val", \
-                    action='store', default=0, type=int, help=' Keyword with '+\
-                    'which to fill masked pixels. Default: 0')                   
+                    'the input.')          
 
 ############################################################################                   
 
