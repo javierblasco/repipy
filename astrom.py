32a8ae2a6437a9664826793f419c757ba049a471
javierblasco <atreo1@hotmail.com>
2015-05-27 22:06:04 +0200

    Add the option to remove cosmic rays before running astrometry.

--- astrom.py
+++ astrom.py
@@ -8,6 +8,7 @@ Created on Tue Jul  9 11:03:50 2013
 
 import repipy.utilities as utilities
 import repipy.astroim as astroim
+import repipy.remove_cosmics as remove_cosmics
 import subprocess
 import os
 import sys
@@ -15,69 +16,116 @@ import shutil
 import tempfile
 import argparse
 import astropy.io.fits as fits
+import astropy.wcs as wcs
+
+
+def get_basename(im_name):
+    """  Given a file name, return the file without the path AND without the extension
+    :param im_name: input filename
+    :return: the same file, but without the path and the extension, for /home/blasco/file,fits --> file
+    """
+    basename = os.path.splitext(os.path.basename(im_name))[0]
+    return basename
+
+
+def perform_cosmic_removal(im_name, output=None):
+    """ Remove cosmics from fits image
+    :param im_name: fits image to be cleaned from cosmic rays
+    :return: name of the cleaned image
+    """
+    if output is None:
+        output = im_name
+
+    arguments = [im_name, "--output", output, "--maxiter", "1"]
+
+    im = astroim.Astroim(im_name)
+
+    needed_keys = [("--gain",im.header.gaink), ("--readnoise",im.header.ccdronk)]
+    for arg, kk in needed_keys:
+        if kk is None:
+            continue
+        arguments += [arg, kk]
+
+    new_name = remove_cosmics.main(arguments=arguments)
+
+    return new_name
+
 
 def include_WCS(args):
-
     # Copy input names into output names
     output_names = args.input[:]
     for ii, im_name in enumerate(args.input):
         # If it is a domeflat, skyflat or bias image, calculating the astrometry makes little sense
         im = astroim.Astroim(im_name)
-        if im.target.objtype in ["bias", "domeflat", "skyflat"]:
+        if im.target.objtype in ["bias", "domeflat", "skyflat", "flat"]:
             continue
 
         # Prepare my output file  for the resulting image
-        new_file = im_name
         if args.suffix:
-            new_file = utilities.add_suffix_prefix(new_file, suffix=args.suffix)
+            new_file = utilities.add_suffix_prefix(im_name, suffix=args.suffix)
+        elif args.overwrite:
+            new_file = im_name
+
+        # Copy input image into a temporary file, so that we can modify it freely, for example, remove cosmics
+        # or filter it (to be done).
+        basename = get_basename(im_name)
+        _, input_image = tempfile.mkstemp(prefix=basename, suffix=".fits")
+        shutil.copy2(im_name, input_image)
 
 
-
-        # And now, to avoid having too much residual crap in the folder, the output of astrometry will go to tmp
-        basename = os.path.basename(im_name)
-        fd1, resultfile = tempfile.mkstemp(prefix=basename, suffix=".newfile")
-        fd2, wcsfile = tempfile.mkstemp(prefix=basename, suffix=".wcs")
-        fd3, xylsfile = tempfile.mkstemp(prefix=basename, suffix=".xyls")
-        fd4, rdlsfile = tempfile.mkstemp(prefix=basename, suffix=".rdls")
-        fd5, axyfile = tempfile.mkstemp(prefix=basename, suffix=".axy")
-        fd6, solutionfile = tempfile.mkstemp(prefix=basename, suffix=".solved")
-        fd7, matchfile = tempfile.mkstemp(prefix=basename, suffix=".match")
-        fd8, corrfile = tempfile.mkstemp(prefix=basename, suffix=".corr")
+        # Remove cosmics
+        if args.cosmics:
+            perform_cosmic_removal(input_image)
 
 
-        # Prepare the arguments of solve-field
-        arguments0 = ["solve-field", "--no-plots", "--no-fits2fits", "--use-sextractor",
-                     "--overwrite", "--new-fits", resultfile, "--wcs", wcsfile, "--index-xyls", xylsfile,
-                     "--rdls", rdlsfile, "--axy", axyfile, "--solved", solutionfile, "--match", matchfile,
-                     "--cpulimit", "120", "--cpulimit", "120", "--corr", corrfile, im_name]
+        # The output of the WCS process of astrometry.net will go to a .new file, the coordinates to a .coor
+        _, output_wcs = tempfile.mkstemp(prefix=basename, suffix=".new")
+        _, corrfile = tempfile.mkstemp(prefix=basename, suffix=".cor")
 
-        # Try to add the RA, DEC, Radius options to constrain the search
-        with utilities.tmp_mute():
-            try:
-                ra, dec = im.header.get( im.header.RAk, im.header.DECk)
-                ra, dec = utilities.sex2deg(ra, dec)
-                arguments = arguments0 + ["--ra", str(ra), "--dec", str(dec), "--radius", str(args.radius)]
-            except:
-                arguments = arguments0
+        # To avoid having too much residual crap in the folder, the output of astrometry will go to tmp (--dir /tmp).
+        arguments0 = ["solve-field", "--no-plots", "--no-fits2fits", "--use-sextractor", "--dir", "/tmp",
+                      "--overwrite", "--new-fits", output_wcs, "--cpulimit", "120", "--cpulimit", "120",
+                      "--tweak-order", "2", "--corr", corrfile, input_image]
+
+        try:  # Try to add the RA, DEC, Radius options to constrain the search
+            ra, dec = im.header.get(im.header.RAk, im.header.DECk)
+            ra, dec = utilities.sex2deg(ra, dec)
+            arguments = arguments0 + ["--ra", str(ra), "--dec", str(dec), "--radius", str(args.radius)]
+        except:
+            arguments = arguments0
 
         # Run astrometry, in case of not solving it on the first attempt, try fitting freely (no RA, DEC used)
         subprocess.call(arguments)
-        if not os.path.exists(solutionfile):
+        if not os.path.exists(output_wcs):
             subprocess.call(arguments0)
 
-        # Only if we have a solution, move resulting files around
-        if os.path.exists(solutionfile):
-            # Move important files around
-            shutil.move(resultfile, new_file)
+        # Only if we have a solution
+        if os.path.exists(output_wcs):
+            # copy the input file into the new file if they are not the same
+            if os.path.abspath(im_name) != os.path.abspath(new_file):
+                shutil.copy2(im_name, new_file)
+
+            # get WCS from the resulting file, only the part added by astrometry.net
+            with fits.open(output_wcs) as file_wcs:
+                hdr_wcs = file_wcs[0].header
+                ind = hdr_wcs.values().index('Original key: "END"')  # new part added by astrometry
+                hdr_wcs = hdr_wcs[ind:]
+
+            # Add that header to the original one of the image
+            with fits.open(new_file, 'update') as new_im:
+                new_im[0].header += hdr_wcs
+                new_im.flush()
+
+            # Build a catalogue of RA, DEC for the stars found
             with fits.open(corrfile) as table:
-                cat_radec = utilities.replace_extension(im_name, "radec")
+                cat_radec = utilities.replace_extension(new_file, "radec")
                 with open(cat_radec, "w") as f:
                     for ra, dec in zip(table[1].data["field_ra"], table[1].data["field_dec"]):
                         f.write(str(ra) + " " + str(dec) + "\n")
             output_names[ii] = new_file
 
             # If --remove_original is True
-            if args.remove_orig:
+            if args.remove_orig and not args.overwrite:
                 os.unlink(im_name)
 
     return output_names
@@ -89,50 +137,57 @@ parser = argparse.ArgumentParser(description=""" Find the WCS for a set of image
 
 
 # Add necessary arguments to parser
-parser.add_argument("input", metavar='input', action='store', help='list of ' +\
-                    'images from which to compute the WCS.', nargs="+", type=str)
-parser.add_argument("--suffix", metavar="suffix", dest='suffix', action='store',\
-                    default='', type=str, help='suffix to be added at the end '+\
-                    'of the image input list to generate the outputs. There '+\
-                    'is a peculiarity with argparse: if you pass, e.g., "-c" to '+\
-                    '--suffix, the program will understand that you want to '+\
-                    'call the code with the flag -c, which does not exist. This '+\
-                    'does not raise an error, but just stops execution, which is '+\
-                    'quite annoying. One way around it is " -c" (notice the '+\
-                    'space, since within the string is stripped. If the --overwrite is used, '+\
-                    'the suffix will be ignored. Use wisely!')
-parser.add_argument("--remove_original",  dest='remove_orig', action="store_true",\
-                     help=' Once the job is done, remove the original image. This will have no effect if '+\
-                          ' --overwrite is used.')
+parser.add_argument("input", metavar='input', action='store', help='list of ' + \
+                                                                   'images from which to compute the WCS.', nargs="+",
+                    type=str)
+parser.add_argument("--suffix", metavar="suffix", dest='suffix', action='store', \
+                    default='', type=str, help='suffix to be added at the end ' + \
+                                               'of the image input list to generate the outputs. There ' + \
+                                               'is a peculiarity with argparse: if you pass, e.g., "-c" to ' + \
+                                               '--suffix, the program will understand that you want to ' + \
+                                               'call the code with the flag -c, which does not exist. This ' + \
+                                               'does not raise an error, but just stops execution, which is ' + \
+                                               'quite annoying. One way around it is " -c" (notice the ' + \
+                                               'space, since within the string is stripped. If the --overwrite is used, ' + \
+                                               'the suffix will be ignored. Use wisely!')
+parser.add_argument("--remove_original", dest='remove_orig', action="store_true", \
+                    help=' Once the job is done, remove the original image. This will have no effect if ' + \
+                         ' --overwrite is used.')
+parser.add_argument("--cosmics", dest='cosmics', action="store_true", \
+                    help=' Prevent some cosmic rays from being detected by astrometry.net. What happens under the hood' + \
+                         'is that we copy the input image into a new one, subtract cosmic rays, solve with '+\
+                         'astrometry.net and add the WCS solution to the original image, which remains otherwise '+\
+                         'unaltered. ')
 parser.add_argument("--overwrite", dest='overwrite', action="store_true",
-                     help='Overwrite the original images. This keyword will override both --remove_original and --suffix')
+                    help='Overwrite the original images. This keyword will override both --remove_original and --suffix')
 parser.add_argument("--radius", dest='radius', action="store", type=float, default="1",
-                     help='Search radius. If the RA and DEC are found in the header, astrometry will look for '+\
-                     'solutions within this radius of those coordinates. Default=1.0')
+                    help='Search radius. If the RA and DEC are found in the header, astrometry will look for ' + \
+                         'solutions within this radius of those coordinates. Default=1.0')
 
 
-def main(arguments = None):
-  # Pass arguments to variable args
-  if arguments == None:
-      arguments = sys.argv[1:]
-  args = parser.parse_args(arguments)
+def main(arguments=None):
+    # Pass arguments to variable args
+    if arguments == None:
+        arguments = sys.argv[1:]
+    args = parser.parse_args(arguments)
 
 
-  # Either --suffix is not "" or --overwrite is active
-  if not args.suffix and not args.overwrite:
-      sys.exit("Declare a --suffix or --overwrite, one of the two must be active!")
+    # Either --suffix is not "" or --overwrite is active
+    if not args.suffix and not args.overwrite:
+        sys.exit("Declare a --suffix or --overwrite, one of the two must be active!")
 
 
-  # If overwrite is used, --suffix and --remove_original have no effect
-  if args.overwrite:
-      args.suffix = ""
-      args.remove_original = False
+    # If overwrite is used, --suffix and --remove_original have no effect
+    if args.overwrite:
+        args.suffix = ""
+        args.remove_original = False
 
-  # If a suffix was passed, check that you remove any blank spaces
-  args.suffix = args.suffix.strip()
+    # If a suffix was passed, check that you remove any blank spaces
+    args.suffix = args.suffix.strip()
 
-  output_list = include_WCS(args)
-  return output_list
+    output_list = include_WCS(args)
+    return output_list
+
 
 if __name__ == "__main__":
     main()
