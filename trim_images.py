666d463a1bf6619602c4e0a30f75a1d2e2ab36b2
javierblasco <atreo1@hotmail.com>
2015-09-15 12:59:43 +0200

    Improve description of argparse

--- trim_images.py
+++ trim_images.py
@@ -40,21 +40,21 @@ def trim(args):
 
 # Create parser
 parser = argparse.ArgumentParser(description='Trim a given section of an image, keeping WCS information true')
-parser.add_argument("input",metavar='input', action='store', nargs="+",  \
+parser.add_argument("input", metavar='input', action='store', nargs="+", \
                     type=str, help='Images to be trimmed.')
 parser.add_argument("--output", metavar='output', action='store', dest='output', \
                     default='', help='Name of output file.')
 parser.add_argument("--suffix", metavar="suffix", dest='suffix', action='store', \
-                    default='',help='suffix to be added at the end of the image' +\
-                    'input list to generate the output.')
-parser.add_argument("--region",metavar=('x0', 'x1', 'y0', 'y1'), action='store', nargs=4, type=int,
-                       required=True, dest="region", \
+                    default='', help='suffix to be added at the end of the image' + \
+                                     'input list to generate the output.')
+parser.add_argument("--region", metavar=('x0', 'x1', 'y0', 'y1'), action='store', nargs=4, type=int,
+                    required=True, dest="region", \
                     help='Region of the image to be trimmed. x0,x1 is the range in the horizontal '
-                          'axis as shown by ds9, while y0, y1 mean the same in the vertical axis.'
-                          'Please, note that what ds9 shows as X (i.e. the horizontal axis) is the second index in '
-                          'a numpy array, the Y axis as shown by ds9 being the first. '
-                          'Usually x0,x1 correspond to Naxis1 and y0,y1 to Naxis2 in the header of fits images. ')
-parser.add_argument("--overwrite", metavar='overwrite', dest='overwrite', action='store_true',
+                         'axis as shown by ds9, while y0, y1 mean the same in the vertical axis.'
+                         'Please, note that what ds9 shows as X (i.e. the horizontal axis) is the second index in '
+                         'a numpy array, the Y axis as shown by ds9 being the first. '
+                         'Usually x0,x1 correspond to Naxis1 and y0,y1 to Naxis2 in the header of fits images. ')
+parser.add_argument("--overwrite", dest='overwrite', action='store_true',
                     help='Overwrite the input file if no suffix or output file is given. ')
 
 def main(arguments=None):
