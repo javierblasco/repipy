7cdaea196db0a8d231914f892be57c83d6e92ea4
javierblasco <atreo1@hotmail.com>
2015-10-05 14:05:41 +0200

    Combine using the WCS information to match the images

--- combine_wcs.py
+++ combine_wcs.py
@@ -11,6 +11,16 @@ with methods.tmp_chdir(repipy.__path__[0]):
     from pyraf import iraf
     from iraf import images
 
+
+class ImList(images):
+    """ List of astronomical FITS images.
+    """
+    def __init__(self):
+        """ Return astropy.io.fits objects
+        """
+
+
+
 def combine(args):
     """ Combine images using the wcs in their headers to match the pixels.
     :param args:
