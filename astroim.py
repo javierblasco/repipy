10e14045688038b86243108ef085ef857880f284
javierblasco <atreo1@hotmail.com>
2015-10-09 12:07:18 +0200

    Read all chips into a HDUlist

--- astroim.py
+++ astroim.py
@@ -8,8 +8,9 @@ class Astroim(object):
     """
     def __init__(self, image):
         self.im_name = image
-        self.header = header.Header(self.im_name)
+        self.header = header.Header(self.im_name, self._HDUList)
         self.filter = imfilter.Filter(self.header)
+        self._HDUList = fits.open(self.im_name, 'readonly')
         self.target = target.Target(self.header, self.filter)
         self.imstats = imstats.Imstats(self)
 
