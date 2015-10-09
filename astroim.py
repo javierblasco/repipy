b7e64e5577718c30b4478f794d61f8f6c9c00f1e
javierblasco <atreo1@hotmail.com>
2015-10-09 12:06:12 +0200

    Avoid shadowing filter, use imfilter instead

--- astroim.py
+++ astroim.py
@@ -1,8 +1,7 @@
 import repipy.header as header
 import repipy.target as target
-import repipy.filter as filter
-import repipy.utilities as utilities
-import numpy
+import repipy.filter as imfilter
+
 
 class Astroim(object):
     """
@@ -10,7 +9,7 @@ class Astroim(object):
     def __init__(self, image):
         self.im_name = image
         self.header = header.Header(self.im_name)
-        self.filter = filter.Filter(self.header)
+        self.filter = imfilter.Filter(self.header)
         self.target = target.Target(self.header, self.filter)
         self.imstats = imstats.Imstats(self)
 
