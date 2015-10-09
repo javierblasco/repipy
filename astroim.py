28661e72d97a2a9572ba74284931a55fec589bca
javierblasco <atreo1@hotmail.com>
2015-10-09 20:40:58 +0200

    Read self._HDUList before calling it

--- astroim.py
+++ astroim.py
@@ -1,7 +1,7 @@
 import repipy.header as header
 import repipy.target as target
 import repipy.filter as imfilter
-
+import astropy.io.fits as fits
 
 class chip(object):
     """ Each of the CCD Header/Data Units (HDUs) of an astronomical image
@@ -18,9 +18,9 @@ class Astroim(object):
     """
     def __init__(self, image):
         self.im_name = image
+        self._HDUList = fits.open(self.im_name, 'readonly')
         self.header = header.Header(self.im_name, self._HDUList)
         self.filter = imfilter.Filter(self.header)
-        self._HDUList = fits.open(self.im_name, 'readonly')
         self.target = target.Target(self.header, self.filter)
         self.imstats = imstats.Imstats(self)
 
