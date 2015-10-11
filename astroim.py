b8c9e8a63fb8163d08d9030508f13c272b6f54f6
javierblasco <atreo1@hotmail.com>
2015-10-11 20:26:20 +0200

    Add a list of objects Chip to the object Astroim.

--- astroim.py
+++ astroim.py
@@ -52,6 +52,7 @@ class Astroim(object):
         self.im_name = image
         self._HDUList = fits.open(self.im_name, 'readonly')
         self.header = header.Header(self._HDUList[0].header)
+        self.chips = [chip(hdu) for hdu in self._HDUList if hdu.data is not None]
         self.filter = imfilter.Filter(self.header)
         self.target = target.Target(self.header, self.filter)
         self.imstats = imstats.Imstats(self)
@@ -59,16 +60,3 @@ class Astroim(object):
     def zero_point(self, aperture=None):
         return self.filter.zero_point(self.target, aperture=aperture)
 
-    @property
-    def data(self):
-        """ Create an array (or a list of arrays, if several chips are present) with all the data in the image
-
-        For each of the objects in the fits image, collect the associated data, if present. At the end, if the image
-        contains only one chip, do not return an list of a single element, return simply the array with the data.
-
-        :return:
-        """
-        data = [ii.data for ii in self._HDUList if ii.data is not None]
-        if len(data) == 1:
-            data = data[0]
-        return data
