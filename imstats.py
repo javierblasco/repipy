5d330cffeeeaf45b4084db848a8bd0d07587809a
javierblasco <atreo1@hotmail.com>
2015-10-14 12:23:02 +0200

    Simplify object initialization. Receive image and mask, no reading, no masking, just statistics.

--- imstats.py
+++ imstats.py
@@ -11,96 +11,35 @@ class Imstats(object):
     with three elements (x0, y0, radius) for circular regions or four elements (x0, y0, x1, y1)
     """
 
-    def __init__(self, astroim, mask_keyword="MASK", region=None):
-        self.im_name = astroim.im_name
-        self.region = region
-        mask_name =  astroim.header._get_value("MASK")
-        self.im_mask = fits.open(mask_name)
-        self.im_data = astroim.data
-        self.user_mask = self._get_user_mask()
-        self.final_mask = numpy.logical_or(self.im_mask, self.user_mask)
-        self.masked_data = numpy.ma.array(self.im_data, mask=self.final_mask)
+    def __init__(self, data, mask=None):
+        self.im_data = numpy.ma.array(data, mask=mask)
 
     def __str__(self):
         """ Print the whole statistical set.
         """
-        variables = [self.im_name, self.mean(), self.std(), self.median(), self.MAD()]
+        variables = [self.mean(), self.std(), self.median(), self.mad()]
         output_message = "Statistics of image {0}: \n Mean = {1}, Std = {2}, Median = {3}, MAD = {4}".format(*variables)
         return output_message
-
-    def _get_user_mask(self):
-        """ From the coordinates passed by the user, build a mask of valid pixels within the region provided.
-
-        If user passed ( (x0, y0), (x1, y1) ) type of region the region should be a rectangle, if
-        """
-        # Default: mask nothing!
-        mask = numpy.zeros_like(self.im_mask)
-        if self.region is None:
-            return mask
-
-        try:
-            ((x0, y0), (x1, y1)) = self.region
-            mask = self._rectangular_mask()
-        except TypeError:
-            pass
-
-        try:
-            ((x0, y0), r0) = self.region
-            mask = self._circular_mask()
-        except TypeError:
-            pass
-
-        return mask
-
-    def _rectangular_mask(self):
-        """ From two points representing two non-consecutive vertices of a rectangle, return a mask
-
-            The points within the rectangle will have value zero, outside the rectangle the mask will have ones.
-            :return: ndarray with the mask
-        """
-        ((x0, y0), (x1, y1)) = self.region
-        if x0 > x1:
-            x0, x1 = x1, x0
-        if y0 > y1:
-            y0, y1 = y1, y0
-
-        mask = numpy.ones_like(self.im_mask)
-        mask[x0:x1+1, y0:y1+1] = 0
-        return mask
-
-    def _circular_mask(self):
-        """ From a point in the image and a radius, build a circular mask
-
-            Inside the circle, the values are zero, outside the mask has ones.
-            :return:  ndarray with the mask
-        """
-        (x0, y0), radius = self.region
-        lenx, leny = self.im_mask.shape
-        y, x = numpy.mgrid[-x0:lenx-x0, -y0:leny-y0]
-
-        mask = numpy.ones_like(self.im_mask)
-        mask[x * x + y * y <= radius * radius] = 0
-        return mask
 
     def mean(self):
         """ Calculate the mean of the non-masked area of an image
         """
-        return numpy.ma.mean(self.masked_data)
+        return numpy.ma.mean(self.im_data)
 
     def std(self):
         """ Calculate the standard deviation of the non-masked area of an image
         """
-        return numpy.ma.std(self.masked_data)
+        return numpy.ma.std(self.im_data)
 
     def median(self):
         """  Calculate the median of the non-masked area of an image
         """
-        return numpy.ma.median(self.masked_data)
+        return numpy.ma.median(self.im_data)
 
     def MAD(self):
         """  Calculate the median absolute deviation of the non-masked area of an image
         """
-        return numpy.ma.median( numpy.ma.abs(self.masked_data - self.median()) )
+        return numpy.ma.median(numpy.ma.abs(self.im_data - self.median()))
 
     def fit_plane(self, model="linear"):
         """  Fit a plane to the non-masked area of an image.
@@ -112,26 +51,26 @@ class Imstats(object):
         y, x = numpy.mgrid[0:lenx, 0:leny]
 
         # Select non-masked elements only
-        x = x[self.final_mask == 0]
-        y = y[self.final_mask == 0]
-        data = self.im_data[self.final_mask == 0]
+        x = x[self.im_data.mask == 0]
+        y = y[self.im_data.mask == 0]
+        data = self.im_data[self.im_data.mask == 0]
 
         # Do the linear regression fit
         X = numpy.array([x, y]).T
         Y = data
 
         # Split train/test samples with a 80% / 20% split
-        X_train, X_test, Y_train, Y_test =  train_test_split(X, Y, test_size=0.2)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
         if model.lower() == "ridge":
-            model = linear_model.RidgeCV(alphas = [0.001, 0.01, 0.1, 1])
+            model = linear_model.RidgeCV(alphas=[0.001, 0.01, 0.1, 1])
         elif model.lower() == "linear":
             model = linear_model.LinearRegression()
 
-        model.fit(X_train,Y_train)
+        model.fit(X_train, Y_train)
 
         # Check residuals with the test sample and print the results
-        residuals = numpy.abs( model.predict(X_test) - Y_test)
+        residuals = numpy.abs(model.predict(X_test) - Y_test)
         output_coefficients = list(model.coef_) + [model.intercept_, numpy.median(residuals), numpy.mean(residuals)]
         output_message = """
                         Fit to a plane:
