df8c2b46345c92681c5971e224739aa752c49822
javierblasco <atreo1@hotmail.com>
2015-10-18 20:00:31 +0200

    Move def get() to another position in the text for clarity

--- header.py
+++ header.py
@@ -62,6 +62,22 @@ class Header(object):
             self.im_name = im_name
         self.hdr = header
 
+    def get(self, *keywords):
+        """ Return the value of a keyword in the header.
+        :param keywords: keywords whose value we want to return
+        :return: value of the keyword given by user
+        """
+        try:
+            result = []
+            for ii in keywords:
+                result.append(self.hdr[ii])
+            if len(result) == 1:
+                return result[0]
+            else:
+                return result
+        except:
+            return None
+
     @property
     def telescope(self):
         return self._get_telescope()[1]
@@ -109,24 +125,6 @@ class Header(object):
     def _get_value(self, keywords):
         """ Try keywords from the dictionary until you find one that exists. Return the value. """
         return self._key_and_value(keywords)[1]
-
-
-    def get(self, *keywords):
-        """ Return the value of a keyword in the header.
-        :param keywords: keywords whose value we want to return
-        :return: value of the keyword given by user
-        """
-        try:
-            result = []
-            for ii in keywords:
-                result.append(self.hdr[ii])
-            if len(result) == 1:
-                return result[0]
-            else:
-                return result
-        except:
-            return None
-
 
     def find_in_header(self, list_keywords, dict_patterns):
         """ Find the keyword in which elements from both lists coincide.
